050fe17ecefbbf573de79ef18d2be9b61a88d796
Christopher Owen <chris.owen@sigmalabs.co.uk>
2024-08-12 09:27:49 +0100

    Add

--- pipeline.py
+++ pipeline.py
@@ -15,15 +15,12 @@ def extract_interviews(fields: dict) -> list:
         if 'Client' in field:
             num = field.split(' ')[-1]
             status = fields.get('Status ' + num)
-            date = None
-            if status == 'Offer':
-                date = fields.get('Offer Date')
             interviews.append({
                 'Email': fields.get('Name'),
+                'Cohort': fields.get('Cohort'),
                 'Company': value,
                 'Outcome': status,
-                'Interview Date': date,
-                'Style of Interview': None
+                'Style of Interview': "Unknown"
             })
     return interviews
 
@@ -34,20 +31,29 @@ def normalise_and_explode_interview(interview: dict):
     if not outcome:
         return []
 
-    if outcome in ['Offer', 'Withdrawn', 'Stage 1', 'S1-Rejected', 'S1-Progress']:
+    if outcome in ['Offer', 'Withdrawn', 'Stage 1', 'S1-Rejected', 'S1-Progress', "Turned down"]:
         interview['Round'] = 'Stage 1'
         if outcome == 'Stage 1':
-            interview['Outcome'] = None
+            interview['Round'] = 'Stage 1'
+            interview['Outcome'] = "Pending"
         elif outcome == 'S1-Rejected':
+            interview['Round'] = 'Stage 1'
             interview['Outcome'] = 'Fail'
         elif outcome == 'S1-Progress':
+            interview['Round'] = 'Stage 1'
             interview['Outcome'] = 'Progress'
+        elif outcome == 'Turned down':
+            interview['Round'] = 'Unknown Stage'
+            interview['Outcome'] = 'Turned down'
+        elif outcome == "Offer":
+            interview['Round'] = 'Unknown Stage'
+            interview['Outcome'] = 'Offer'
         return []
 
     if 'Stage' in outcome:
-        stage_num = int(outcome.split(" "))
+        stage_num = int(outcome.split(" ")[1])
         interview['Round'] = outcome
-        interview['Outcome'] = None
+        interview['Outcome'] = "Pending"
     else:
         stage_num = int(outcome.split("-")[0][1:])
         interview['Round'] = f'Stage {stage_num}'
@@ -55,12 +61,12 @@ def normalise_and_explode_interview(interview: dict):
             interview['Outcome'] = 'Fail'
         else:
             interview['Outcome'] = 'Progress'
-    
+
     previous_interview = {}
     for (field, value) in interview.items():
         previous_interview[field] = value
     previous_interview['Outcome'] = f'S{stage_num - 1}-Progress'
-    
+
     return [previous_interview]
 
 
@@ -68,6 +74,8 @@ def main():
     api = Api(ACCESS_TOKEN)
     summary_table = api.table(BASE_ID, 'Summary')
     tracker_table = api.table(BASE_ID, 'Interview Tracker')
+
+    print(ACCESS_TOKEN, BASE_ID)
 
     summary_records = []
     table_iter = summary_table.iterate()
@@ -85,13 +93,14 @@ def main():
         interviews += normalise_and_explode_interview(interview)
 
     interviews = filter(lambda interview: interview['Outcome'], interviews)
-    interviews = sorted(interviews, key= lambda interview: [interview['Email'], interview['Round']])
+    interviews = sorted(interviews, key=lambda interview: [
+                        interview['Email'], interview['Round']])
 
     # Less efficient than batch_create, but keeps the records in a logical order in views!
     for interview in interviews:
+        print(interview)
         print(tracker_table.create(interview, typecast=True))
 
 
 if __name__ == "__main__":
     main()
-
